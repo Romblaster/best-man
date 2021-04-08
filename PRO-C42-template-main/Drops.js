72fbd0954a89c758b824403f22f4e9d9bb1f2a5f
Romblaster <76746431+Romblaster@users.noreply.github.com>
2021-04-08 08:55:48 -0400

    Update Drops.js

--- PRO-C42-template-main/Drops.js
+++ PRO-C42-template-main/Drops.js
@@ -4,22 +4,21 @@ class Drops{
             'restitution':0.1,
             'friction':0.1
         }
-        this.body = Bodies.circle(x,y,5,options)
-        this.radius = 5
-        World.add(world, this.body)
+        this.body = Bodies.circle(x,y,5,options);
+        this.radius = 5;
+        World.add(world, this.body);
     }
 
-update(){
-    if(this.body.position.y> height){
-        Matter.Body.setPosition(this.body,{x:random(0,400), y:random(0,400)})
+    update(){
+        if(this.body.position.y> height){
+            Matter.Body.setPosition(this.body,{x:random(0,400), y:random(0,400)});
+        }
     }
-}
 
-display(){
-    fill(0,0,255)
-    ellipseMode(RADIUS)
-    ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius)
-    
-}
-
+    display(){
+        fill(0,0,255);
+        ellipseMode(RADIUS);
+        ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+        
+    }
 }
