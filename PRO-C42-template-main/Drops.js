5551bd300a7a0cdb66012f8c8324decc640f9e5c
Romblaster <76746431+Romblaster@users.noreply.github.com>
2021-04-08 08:42:45 -0400

    Update Drops.js

--- PRO-C42-template-main/Drops.js
+++ PRO-C42-template-main/Drops.js
@@ -11,7 +11,7 @@ class Drops{
 
 update(){
     if(this.body.position.y> height){
-        Matter.body.setPosition(this.body,{x:random(0,400), y:random(0,400)})
+        Matter.Body.setPosition(this.body,{x:random(0,400), y:random(0,400)})
     }
 }
 
